515c1fde4721c36a84f377420d10f5854e3f6e15
Jibreel <xdarkmemoriez@gmail.com>
2024-08-27 16:14:20 +0100

    create cli from newly initialised tailwindcss parser lib

---
+++ go.mod
@@ -0,0 +1,7 @@
+module tailwindcssparser
+
+go 1.22.5
+
+require github.com/sgsavu/tailwindcssparser v0.1.0
+
+require golang.org/x/exp v0.0.0-20240823005443-9b4947da3948 // indirect
---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	"github.com/sgsavu/tailwindcssparser"
+)
+
+func main() {
+	var minify = flag.Bool("minify", false, "Minifies the parsed css.")
+	flag.Parse()
+	if len(flag.Args()) < 1 {
+		fmt.Println("Example usage: ./tailwind-parser --minify \"w-24\"")
+		return
+	}
+	tailwindTags := flag.Arg(0)
+
+	parsedTailwind, err := tailwindcssparser.GetParsedTailwind(tailwindTags, minify)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(parsedTailwind)
+}
